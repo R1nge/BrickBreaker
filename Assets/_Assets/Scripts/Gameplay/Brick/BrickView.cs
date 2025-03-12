655b5940c2a700ef81f29a435f7f85295beedeb1
R1nge <r1ngegamedev@gmail.com>
2025-03-12 11:22:17 +0300

    feat(brick): destroy on collision

--- Assets/_Assets/Scripts/Gameplay/Brick/BrickView.cs
+++ Assets/_Assets/Scripts/Gameplay/Brick/BrickView.cs
@@ -1,9 +1,21 @@
-﻿using UnityEngine;
+﻿using _Assets.Scripts.Gameplay.Ball;
+using UnityEngine;
 
 namespace _Assets.Scripts.Gameplay.Brick
 {
 	public class BrickView : MonoBehaviour
 	{
+		private void OnCollisionEnter2D(Collision2D other)
+		{
+			if (other.gameObject.TryGetComponent(out BallView ballView))
+			{
+				Destroy();
+			}
+		}
 
+		private void Destroy()
+		{
+			Destroy(gameObject);
+		}
 	}
 }
