dc223135623a1397715e1f7f21f9e36ed7539e3c
R1nge <r1ngegamedev@gmail.com>
2025-03-12 13:33:35 +0300

    feat(sounds): degen sounds; random sound picker

--- Assets/_Assets/Scripts/Gameplay/Brick/BrickView.cs
+++ Assets/_Assets/Scripts/Gameplay/Brick/BrickView.cs
@@ -12,6 +12,7 @@ namespace _Assets.Scripts.Gameplay.Brick
 		[SerializeField] private Color32[] colors;
 		[SerializeField] private int hitsToDestroy;
 		[SerializeField] private int points;
+		[SerializeField] private AudioSource audioSource;
 		[Inject] private BrickAmountChecker _brickAmountChecker;
 		private int _currentHits;
 		[Inject] private ScoreHolder _scoreHolder;
@@ -28,6 +29,7 @@ namespace _Assets.Scripts.Gameplay.Brick
 			{
 				_scoreHolder.AddPoints(points);
 				_brickAmountChecker.Remove(1);
+				Instantiate(audioSource, transform.position, Quaternion.identity);
 				Destroy(gameObject);
 			}
 		}
---
+++ Assets/_Assets/Scripts/Gameplay/RandomSoundPicker.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace _Assets.Scripts.Gameplay
+{
+	public class RandomSoundPicker : MonoBehaviour
+	{
+		[SerializeField] private AudioSource audioSource;
+		[SerializeField] private AudioClip[] clips;
+		private int _lastIndex;
+
+		private void Awake()
+		{
+			var clip = clips[Random.Range(0, clips.Length)];
+			audioSource.clip = clip;
+			audioSource.Play();
+			Destroy(gameObject, clip.length);
+		}
+	}
+}
