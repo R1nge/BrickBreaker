dc40fd19139610edcc9ce3dbb0bc64e8238e8ca6
R1nge <r1ngegamedev@gmail.com>
2025-03-12 11:17:50 +0300

    feat(ball): bounce

--- Assets/_Assets/Scripts/Gameplay/Ball/BallView.cs
+++ Assets/_Assets/Scripts/Gameplay/Ball/BallView.cs
@@ -4,6 +4,19 @@ namespace _Assets.Scripts.Gameplay.Ball
 {
 	public class BallView : MonoBehaviour
 	{
-		
+		[SerializeField] private new Rigidbody2D rigidbody2D;
+		[SerializeField] private float reflectionForce = 500f;
+		[SerializeField] private float gravityForce;
+		private Vector3 _lastFrameVelocity;
+
+		private void Update() => _lastFrameVelocity = rigidbody2D.velocity;
+
+		private void FixedUpdate() => rigidbody2D.AddForce(Vector2.down * (gravityForce * rigidbody2D.mass));
+
+		private void OnCollisionEnter2D(Collision2D other)
+		{
+			var reflectionDirection = Vector2.Reflect(_lastFrameVelocity, other.contacts[0].normal);
+			rigidbody2D.AddForce(reflectionDirection * reflectionForce * rigidbody2D.mass, ForceMode2D.Impulse);
+		}
 	}
 }
